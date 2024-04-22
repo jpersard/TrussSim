93c639a6aa2310e7f8d4524d719ab0f296cc91b3
Julian Persard <julian@persard.de>
2024-04-22 18:34:00 +0200

    split code in different files for functions and classes

---
+++ classes.py
@@ -0,0 +1,61 @@
+# Classes for TrussSim
+
+
+class Node:
+    """Class representing a node in the truss structure."""
+
+    def __init__(self, x, y):
+        """
+        Initialize a Node object.
+
+        Parameters:
+            x (float): x-coordinate of the node.
+            y (float): y-coordinate of the node.
+        """
+        self.x = x 
+        self.y = y
+
+class Connection:
+    """Class representing a connection (edge) between two nodes in the truss structure."""
+
+    def __init__(self, node1, node2):
+        """
+        Initialize a Connection object.
+
+        Parameters:
+            node1 (Node): First node connected by the connection.
+            node2 (Node): Second node connected by the connection.
+        """
+        self.node1 = node1
+        self.node2 = node2
+
+class Support:
+    """Class representing a support node in the truss structure."""
+
+    def __init__(self, node_index, support_type):
+        """
+        Initialize a Support object.
+
+        Parameters:
+            node_index (int): Index of the supported node.
+            support_type (str): Type of support (e.g., 'pin' or 'roller').
+        """
+        self.node_index = node_index
+        self.support_type = support_type
+
+class Load:
+    """Class representing a load applied to a node in the truss structure."""
+
+    def __init__(self, node_index, force_x, force_y):
+        """
+        Initialize a Load object.
+
+        Parameters:
+            node_index (int): Index of the node where the load is applied.
+            force_x (float): Magnitude of the force in the x-direction.
+            force_y (float): Magnitude of the force in the y-direction.
+        """
+        self.node_index = node_index
+        self.force_x = force_x
+        self.force_y = force_y
+
--- main.py
+++ main.py
@@ -1,65 +1,22 @@
 import matplotlib.pyplot as plt
+import classes
+import functions
 
-class Node:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-class Connection:
-    def __init__(self, node1, node2):
-        self.node1 = node1
-        self.node2 = node2
-
-class Support:
-    def __init__(self, node_index, support_type):
-        self.node_index = node_index
-        self.support_type = support_type
-
-class Load:
-    def __init__(self, node_index, force_x, force_y):
-        self.node_index = node_index
-        self.force_x = force_x
-        self.force_y = force_y
 
 # define truss nodes
 nodes_data = [(0, 0), (3, 2), (3, 0), (6, 0)]
-nodes = [Node(x, y) for x, y in nodes_data]
+nodes = [classes.Node(x, y) for x, y in nodes_data]
 
 # define connections
 connections_data = [(0, 1),(0, 2), (1, 2), (1, 3),(2, 3)]
-connections = [Connection(nodes[i], nodes[j]) for i, j in connections_data]
+connections = [classes.Connection(nodes[i], nodes[j]) for i, j in connections_data]
 
 # Define supports
 supports_data = {0: 'pin', 3: 'roller'}
-supports = [Support(node_index, support_type) for node_index, support_type in supports_data.items()]
+supports = [classes.Support(node_index, support_type) for node_index, support_type in supports_data.items()]
 
 # Define loads
 loads_data = [(2, 0, -100)]
-loads = [Load(node_index, force_x, force_y) for node_index, force_x, force_y in loads_data]
+loads = [classes.Load(node_index, force_x, force_y) for node_index, force_x, force_y in loads_data]
 
-
-# Plot truss
-for connection in connections:
-    node1 = connection.node1
-    node2 = connection.node2
-    plt.plot([node1.x, node2.x], [node1.y, node2.y], 'k-')
-
-# Plot supports
-for support in supports:
-    node = nodes[support.node_index]
-    if support.support_type == 'roller':
-        plt.plot(node.x, node.y, 'ro')
-    elif support.support_type == 'pin':
-        plt.plot(node.x, node.y, 'bo')
-
-# Plot loads
-for load in loads:
-    node = nodes[load.node_index]
-    plt.arrow(node.x, node.y, load.force_x/10, load.force_y/10, head_width=0.1, head_length=0.1, fc='g', ec='g')
-
-plt.xlabel('X-Axis')
-plt.ylabel('Y-Axis')
-plt.title('Truss Structure')
-plt.grid(True)
-plt.gca().set_aspect('equal', adjustable='box')
-plt.show()
+functions.plot_truss_structure(connections, supports, nodes, loads)
