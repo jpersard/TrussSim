b63f9149414602d2cc3363cd5668f35da3cb8e31
Julian Persard <julian@persard.de>
2024-04-23 22:20:16 +0200

    working on coeff matrix

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # main.py
 import functions
+import numpy as np
+
 
 # Variables to enable modules
 # Later to be set in GUI
 do_import = True
 do_reaction = True
-do_print_all = True
+do_print_all = False
 do_plot_truss = False
 
 # Import truss data from JSON
@@ -13,7 +15,7 @@ if do_import:
     json_path = r'C:\dev\TrussSim\truss_data.json'
     nodes, connections, supports, loads = functions.import_json(json_path)
 
-# Calculate reaction forces
+# Calculate forces
 if do_reaction:
     # Initialize reaction forces
     reaction_forces = []
@@ -25,6 +27,30 @@ if do_reaction:
         print("Truss is not statically determined!")
         input("Press Enter to continue...")
         exit()
+
+    # Initialize the coefficient matrix with zeros
+    coefficient_matrix = np.zeros((2 * len(nodes), 2 * len(nodes)))
+    # Populate the coefficient matrix based on connections
+
+    # Print the coefficient matrix
+    print(coefficient_matrix)
+
+    if False:
+        coefficient_matrix = np.array([[1,  0,  0,  0.83,   1,   0,     0,  0], 
+                                    [0,  1,  0,  0.55,   1,   0,     0,  0], 
+                                    [0,  0,  0,  0.83,   0,  -1, -0.55,  0], 
+                                    [0,  0,  0,  0.55,   0,  -1, -0.55,  0], 
+                                    [0,  0,  0,     0,   1,   0,     0,  1], 
+                                    [0,  0,  0,     0,   0,  -1,     0,  0],
+                                    [0,  0,  0,     0,   0,   0,  0.83,  1], 
+                                    [0,  0,  1,     0,   0,   0, -0.55,  0]])
+
+        # Constant matrix
+        constant_matrix = np.array([0, 0, 0, 0, 0, -100, 0, 0])
+
+        # Solve for the variables
+        variables = np.linalg.solve(coefficient_matrix, constant_matrix)
+        print(variables)
 
 # Print all objects
 if do_print_all:
