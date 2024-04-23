ed961f1e1a7239c1d764c2421da9952304823f3e
Julian Persard <julian@persard.de>
2024-04-23 14:51:48 +0200

    equation system for reaction forces

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # main.py
 import matplotlib.pyplot as plt
 import functions
+import classes
 import json
 import numpy as np
 
@@ -12,23 +13,51 @@ with open(truss_data) as file:
 
 nodes, connections, supports, loads = functions.import_json(json_data)
 
+print("Nodes:")
 for node in nodes:
     print(node)
+print("\n")
 
+print("Connections:")
 for connection in connections:
     print(connection)
+print("\n")
 
+print("Supports:")
 for support in supports:
     print(support)
+print("\n")
 
+print("Loads:")
 for load in loads:
     print(load)
+print("\n")
 
 # Calculate reaction forces
-coefficients_matrix = np.array([[1, 0, 0], [0, 1, 1], [0, 0, 6]])
-constants_vector = np.array([0, 100, 100*3])
-reaction_forces = np.linalg.solve(coefficients_matrix, constants_vector)
-print(reaction_forces)
+reaction_forces = []
+# Iterate through supports to populate reaction forces
+for support in supports:
+    # For pin support, create a Reaction object with x and y components
+    if support.support_type == 'pin':
+        reaction_force = classes.Reaction(support.node_index, 1, 1)
+    # For roller support, create a Reaction object with only y component
+    elif support.support_type == 'roller':
+        reaction_force = classes.Reaction(support.node_index, 0, 1)
+    reaction_forces.append(reaction_force)
+
+#coefficients_matrix = np.array([[1, 0, 0], 
+#                                [0, 1, 1], 
+#                               [0, 0, 6]])
+#constants_vector = np.array([0, 
+#                             100, 
+#                             100*3])
+#reaction_forces = np.linalg.solve(coefficients_matrix, constants_vector)
+
+print("Reaction forces:")
+for force in reaction_forces:
+    print(force)
+print("\n")
 
 # Plot the truss structure with loads and forces
-functions.plot_truss_structure(connections, supports, nodes, loads)
+if 0:
+    functions.plot_truss_structure(connections, supports, nodes, loads)
